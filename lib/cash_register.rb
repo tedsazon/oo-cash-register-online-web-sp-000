dd4685ed9fb0bab35ea5c1963dece825edc86460
Ted Sazon <tedsazon@gmail.com>
2019-01-03 19:17:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,5 @@
 class CashRegister
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, :title
 
   def initialize(discount = 0)
     @total = 0
@@ -10,7 +10,7 @@ class CashRegister
     @total
   end
 
-
+  def add_item(title, price)
 
 
 end
